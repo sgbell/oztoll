10891015368f3a4b50b5534de0116700e1bba74d
Sam Bell <bugmanaus@gmail.com>
2011-09-09 10:50:03 +1000

    * Start of the OzToll Project

---
+++ src/com/bg/oztoll/OzTollActivity.java
@@ -0,0 +1,13 @@
+package com.bg.oztoll;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OzTollActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
