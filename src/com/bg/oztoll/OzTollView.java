02c8b893836c4638c169e04d74b953a61907d464
Sam Bell <bugmanaus@gmail.com>
2011-10-01 11:35:49 +1000

    Setting up initial program code

---
+++ src/com/bg/oztoll/OzTollView.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.bg.oztoll;
+
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.SurfaceHolder;
+
+/**
+ * @author bugman
+ *
+ */
+public class OzTollView extends SurfaceView implements SurfaceHolder.Callback {
+
+	public OzTollView(Context context) {
+		super(context);
+		getHolder().addCallback(this);
+		setFocusable(true);
+	}
+	
+	public void onDraw(Canvas canvas){
+		canvas.drawColor(Color.WHITE);
+	}
+	
+	public boolean onTouchEvent(MotionEvent event){
+		
+		return false;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
