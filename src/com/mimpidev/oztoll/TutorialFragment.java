477058f4b76e02a9edaac0b5e6c947196bd66330
Sam Bell <bugmanaus@gmail.com>
2013-02-09 18:10:27 +1100

    * cleaned up imports

--- src/com/mimpidev/oztoll/TutorialFragment.java
+++ src/com/mimpidev/oztoll/TutorialFragment.java
@@ -6,17 +6,12 @@ package com.mimpidev.oztoll;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-
 import com.actionbarsherlock.app.SherlockFragment;
 
 /**
