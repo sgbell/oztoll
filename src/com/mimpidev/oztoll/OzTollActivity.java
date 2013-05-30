7172aa322a362880458f647628ebfc59411e452c
Sam Bell <bugmanaus@gmail.com>
2013-05-30 10:20:26 +1000

    * Removing Log reference

--- src/com/mimpidev/oztoll/OzTollActivity.java
+++ src/com/mimpidev/oztoll/OzTollActivity.java
@@ -23,7 +23,6 @@ import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentTransaction;
 import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
